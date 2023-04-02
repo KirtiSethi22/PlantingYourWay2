72d2d9fd1907152e9a12596d4d8c6e75b5c54560
Kirti <kirtisethi22@gmail.com>
2023-04-01 20:11:43 -0700

    V2 changes

---
+++ app/src/main/java/com/example/plantingyourway/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.plantingyourway;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setLogo(R.mipmap.ic_launcher_logo_round);
+        actionBar.setTitle("Planting Your Way");
+    }
+}
